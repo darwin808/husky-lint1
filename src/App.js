8757f9712e88b4c098a35d23941b2f5ecf59f07c
darwin <darwin.apolinario@serino.com>
2021-08-23 12:41:22 +0800

    awdq

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  /*   const test = "qweqwe"; */
+  const test = "qweqwe";
   return (
     <div className="app">
       <header className="App-header">
